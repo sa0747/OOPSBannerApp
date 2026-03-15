882d0e40bb984be5dc98181bd79424b90c18ea9a
sa0747 <sa0747@srmist.edu.in>
2026-03-16 00:47:05 +0530

    UC4: Render OOPS banner using String Array and Loop

---
+++ OOPSBannerUC4.java
@@ -0,0 +1,33 @@
+public class OOPSBannerUC4 {
+
+    public static void main(String[] args) {
+
+        String[] O = {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+
+        String[] P = {
+                "*****  ",
+                "*    * ",
+                "*****  ",
+                "*      ",
+                "*      "
+        };
+
+        String[] S = {
+                " ***** ",
+                "*      ",
+                " ***** ",
+                "      *",
+                " ***** "
+        };
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(O[i] + "  " + O[i] + "  " + P[i] + "  " + S[i]);
+        }
+    }
+}
